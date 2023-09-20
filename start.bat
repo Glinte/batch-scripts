fca5bfd8d026d62aee3c38ad01d4688b8821ef67
Savio Mak <96855131+Glinte@users.noreply.github.com>
2023-09-20 08:51:46 +0800

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,5 @@
+start "" "F:\windows\Snipaste-2.8.5-Beta-x64\Snipaste.exe"
+start "" "F:\windows\Stretchly\Stretchly.exe"
+start "" "F:\windows\FirefoxPortable\FirefoxPortable.exe"
+start "" "C:\Users\user\AppData\Local\PowerToys\PowerToys.exe"
+start "" "F:\windows\Google Search_v2.ahk"
